d26e21da3b647d5fc80237b963ad7bc2f2613abb
Axel Hernández Ferrera <axelhzf@gmail.com>
2013-10-13 17:48:48 +0100

    travis test

--- test/Calculator.test.js
+++ test/Calculator.test.js
@@ -1,6 +1,6 @@
 describe('Calculator', function () {
     it('should add', function () {
         var calculator = new Calculator();
-        expect(calculator.add(1, 2)).to.equal(3);
+         expect(calculator.add(1, 2)).to.equal(3);
     });
 });
